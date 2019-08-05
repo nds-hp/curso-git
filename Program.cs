8f430b9297a8fe6b138d8a9b0eea77f175e10168
Nilton Santos <ndshpelite@outlook.com>
2019-08-05 15:02:24 -0300

    adiconada a pergunta como es´tá hoje?

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace Project_Test {
             Console.WriteLine("Hello World!");
             for (int i = 0; i < 10; i++) {
                 Console.WriteLine("oi");
+              
             }
+            Console.WriteLine("Como está hoje?");
         }
     }
 }
